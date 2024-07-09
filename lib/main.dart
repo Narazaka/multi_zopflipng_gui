d17513169f14089c667b3e80184bb8a633bd92dd
Narazaka <info@narazaka.net>
2024-07-10 03:38:17 +0900

    impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:filesize/filesize.dart';
+import 'package:flutter/material.dart';
+import 'package:desktop_drop/desktop_drop.dart';
+import 'package:path/path.dart' as p;
+import 'package:queue/queue.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
   runApp(const MyApp());
 }
 
@@ -11,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'multi zopflipng',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'multi zopflipng'),
     );
   }
 }
@@ -54,18 +61,119 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class EntryInfo {
+  final String path;
+  final int before;
+  bool processing = false;
+  int? after;
+  int? get reduced => isProcessed ? before - after! : null;
+  double? get reducedRate => isProcessed ? reduced! / before : null;
+  bool get isProcessed => after != null;
+  String get beforeSize => filesize(before);
+  String get afterSize => isProcessed ? filesize(after!) : "";
+  String get reducedSize => isProcessed ? "-${filesize(reduced!)}" : "";
+  String get reducedPercent =>
+      isProcessed ? "-${(reducedRate! * 100).toStringAsFixed(2)}" : "";
 
-  void _incrementCounter() {
+  EntryInfo(this.path, this.before);
+}
+
+class _MyHomePageState extends State<MyHomePage> with WindowListener {
+  final Queue _queue = Queue(parallel: Platform.numberOfProcessors ~/ 2);
+  final List<EntryInfo> _entries = [];
+  final Set<Process> _processes = {};
+  bool _m = true;
+  bool _lossyTransparent = true;
+  bool _lossy8bit = false;
+
+  void _addEntries(DropDoneDetails details) async {
+    var addFiles = List<EntryInfo>.empty(growable: true);
+    for (var f in details.files) {
+      if (await FileSystemEntity.isDirectory(f.path)) {
+        await for (var ff in Directory(f.path).list(recursive: true)) {
+          if (await FileSystemEntity.isFile(ff.path) &&
+              p.extension(ff.path) == ".png") {
+            addFiles.add(EntryInfo(ff.path, await File(ff.path).length()));
+          }
+        }
+      } else if (p.extension(f.path) == ".png") {
+        addFiles.add(EntryInfo(f.path, await File(f.path).length()));
+      }
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _entries.addAll(addFiles);
     });
+    for (var e in addFiles) {
+      _queue.add(() async {
+        var args = <String>[];
+        if (_m) {
+          args.add("-m");
+        }
+        if (_lossyTransparent) {
+          args.add("--lossy_transparent");
+        }
+        if (_lossy8bit) {
+          args.add("--lossy_8bit");
+        }
+        args.add("-y");
+        args.add(e.path);
+        args.add(e.path);
+        setState(() {
+          e.processing = true;
+        });
+        var process = await Process.start("zopflipng.exe", args);
+        _processes.add(process);
+        if (await process.exitCode != 0) {
+          print(process.stderr);
+        }
+        _processes.remove(process);
+        var after = await File(e.path).length();
+        setState(() {
+          e.processing = false;
+          e.after = after;
+        });
+      });
+    }
+  }
+
+  String _title() {
+    if (_entries.isEmpty) {
+      return "ready";
+    }
+    var processedEntries =
+        _entries.where((e) => e.isProcessed).toList(growable: false);
+    var totalBefore = _entries.fold(0, (p, e) => p + e.before);
+    var before = processedEntries.fold(0, (p, e) => p + e.before);
+    var after = processedEntries.fold(0, (p, e) => p + e.after!);
+    var reduced = before - after;
+    var reducedRate = before == 0 ? 0 : reduced / before;
+    var reducedPercent = (reducedRate * 100).toStringAsFixed(2);
+    return "${processedEntries.length} / ${_entries.length} | total: ${filesize(totalBefore)} ${filesize(before)} -> ${filesize(after)} (-${filesize(reduced)} / $reducedPercent%)";
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    windowManager.addListener(this);
+    _init();
+  }
+
+  void _init() async {
+    await windowManager.setPreventClose(true);
+  }
+
+  @override
+  void onWindowClose() async {
+    for (var p in _processes) {
+      p.kill(ProcessSignal.sigkill);
+    }
+    await windowManager.destroy();
   }
 
   @override
@@ -76,50 +184,122 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return DropTarget(
+      onDragDone: _addEntries,
+      child: Scaffold(
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(_title()),
         ),
+        body: Column(children: [
+          Row(
+            children: [
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 100,
+                child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'concurrency',
+                    ),
+                    controller:
+                        TextEditingController(text: _queue.parallel.toString()),
+                    // initialValue: _queue.parallel.toString(),
+                    //keyboardType: TextInputType.number,
+                    //inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    onChanged: (v) => setState(() {
+                          _queue.parallel = int.parse(v);
+                        })),
+              ),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 70,
+                child: CheckboxListTile(
+                    controlAffinity: ListTileControlAffinity.leading,
+                    contentPadding: const EdgeInsetsDirectional.all(0),
+                    title: Transform.translate(
+                        offset: const Offset(-10, 0), child: const Text("-m")),
+                    value: _m,
+                    onChanged: (v) => setState(() {
+                          _m = v!;
+                        })),
+              ),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 200,
+                child: CheckboxListTile(
+                    controlAffinity: ListTileControlAffinity.leading,
+                    contentPadding: const EdgeInsetsDirectional.all(0),
+                    title: Transform.translate(
+                        offset: const Offset(-10, 0),
+                        child: const Text("--lossy_transparent")),
+                    value: _lossyTransparent,
+                    onChanged: (v) => setState(() {
+                          _lossyTransparent = v!;
+                        })),
+              ),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 140,
+                child: CheckboxListTile(
+                    controlAffinity: ListTileControlAffinity.leading,
+                    contentPadding: const EdgeInsetsDirectional.all(0),
+                    title: Transform.translate(
+                        offset: const Offset(-10, 0),
+                        child: const Text("--lossy_8bit")),
+                    value: _lossy8bit,
+                    onChanged: (v) => setState(() {
+                          _lossy8bit = v!;
+                        })),
+              ),
+            ],
+          ),
+          _entries.isEmpty
+              ? Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      Text(
+                        'Drop files here!',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      )
+                    ]))
+              : Expanded(
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: SizedBox(
+                          width: double.infinity,
+                          child: DataTable(
+                            columns: const [
+                              DataColumn(label: Text("file")),
+                              DataColumn(label: Text("before")),
+                              DataColumn(label: Text("after")),
+                              DataColumn(label: Text("reduced")),
+                              DataColumn(label: Text("reduced %")),
+                            ],
+                            rows: _entries
+                                .map((f) => DataRow(
+                                        color: f.processing
+                                            ? WidgetStateProperty.resolveWith(
+                                                (states) {
+                                                return Colors.yellow;
+                                              })
+                                            : null,
+                                        cells: [
+                                          DataCell(Text(f.path)),
+                                          DataCell(Text(f.beforeSize)),
+                                          DataCell(Text(f.afterSize)),
+                                          DataCell(Text(f.reducedSize)),
+                                          DataCell(Text(f.reducedPercent)),
+                                        ]))
+                                .toList(),
+                          ))))
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
